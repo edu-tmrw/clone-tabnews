c60b1fea4fc43467d43b5ad126634a53036563e9
Edu Santos <edumatheus@me.com>
2026-01-03 01:03:30 +0000

    Texto centralizado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Te amo, meu bem! ❤️</h1>
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            <h1>Te amo, meu bem! ❤️</h1>
+        </div>
+    );
 }
 
 export default Home;
