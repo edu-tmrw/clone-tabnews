bb7f3e03013416ca8bb853ce5b6467dbfc63922a
Edu Santos <edumatheus@me.com>
2026-02-11 21:09:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 function Home() {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-            }}
-        >
-            <h1>Te amo, meu bem! ❤️</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Te amo, meu bem! ❤️</h1>
+    </div>
+  );
 }
 
 export default Home;
